cf65653f59502141fc6fcb17cb17af27cc55b415
mapoga <mathieugouletaubin@gmail.com>
2014-11-11 18:44:58 -0500

    sequence_to_compact_string

--- file_sequence.py
+++ file_sequence.py
@@ -1,5 +1,7 @@
 import re
 
+WHITE_SPACES = re.compile(r'\s+')
+
 class Sequence:
 
 	def __init__(self, data):
@@ -32,67 +34,82 @@ class Sequence:
 		pass
 
 
-
-def sequence_to_range( numbers=[] ): #String
-	'''
-	Takes a list of numbers as argument and returns a string of the compressed range
-	1,4-6,10,15-18,20-26x2,27,28,30-50x5,56,63-72x3
-	'''
+	
+	
+def sequence_to_compact_string(arg = []):
+	"""
+	Converts a sequence of numbers into a range string representation
+	sequence_to_range([1,2,3,11,12,20,22,24,25]) >> "1-3,11,12,20-24x2,25"
+	"""
+	# Verify arg type, remove duplicates & sort
 	try:
-		for n in numbers:
-			int(n)
+		numbers = sorted(list(set([int(n) for n in arg])))
 	except ValueError:
-		raise ValueError("Sequence must contain numbers")
+		raise ValueError("Sequence must contain only numbers")
 	except TypeError:
-		raise TypeError("Sequence must be a list")
-	numbers = list(set(numbers))
-	numbers.sort()
-	stacks = []
-	lastStep = 0
-
-	def format_ruled_sequence(seq):
-		"""
-		Converts a range into "N-NxN" format
-		"""
-		if len(seq) < 2:
-			return "{0}".format(seq[0])
+		raise TypeError("Sequence must be an iterable")
+	
+	def format_rule(start, end, step):
+		"""  format rule as 'N', N-N or N-NxN  """
+		if  end-start == 0:
+			return "{0}".format(start)
+		elif step == 1:
+			return "{0}-{1}".format(start, end)
 		else:
-			step = seq[1]-seq[0]
-			if step ==1:
-				return "{0}-{1}".format(seq[0], seq[-1])
+			return "{0}-{1}x{2}".format(start, end, step)
+			
+	def sub_rule(numbers = []):
+		"""  returns list of sub sequence defined by a rule   """
+		start = end = numbers[0]
+		step = None
+		for i, n in enumerate(numbers[1:]):
+			if step == None or n-end == step:
+				# continue the loop until step is different
+				step = n-end
+				end = n
 			else:
-				return "{0}-{1}x{2}".format(seq[0], seq[-1], step)
-
-	if numbers:
-		# stack sub-sequences in individual list
-		for i, n in enumerate(numbers):
-			if i == 0:
-				#First number, create a stack
-				stacks.append([n])
+				if step == end-start:
+					# sub_rule is only two numbers long,
+					# yield and start from current
+					yield start, start, step
+					start = end
+					end = n
+					step = end-start				
+				else:
+					# yield sequence
+					yield start, end, step
+					start = end = n
+					step = None
+		else:
+			# last iteration
+			if step == end-start:
+				# sub_rule is only two numbers long,
+				# yield separate
+				yield start, start, step
+				yield end, end, step
 			else:
-				# get step from back and front
-				back = numbers[i]-numbers[i-1]
-				if i != len(numbers)-1:
-					front = numbers[i+1]-numbers[i]
-				else:
-					front = 0
+				# yield single or sequence
+				yield start, end, step
+	return ','.join([format_rule(*i) for i in sub_rule(arg)])
+	
+def compact_string_to_sequence(string=""):
 
-				if back == lastStep:
-					stacks[-1].append(n)
-				elif front == back:
-					if len(stacks[-1]) < 2:
-						lastStep = back
-						stacks[-1].append(n)
-					else:
-						lastStep = back
-						stacks.append([n])
-				else:
-					lastStep = back
-					stacks.append([n])
-
-		return ','.join(format_ruled_sequence(e) for e in stacks)
-	return ""
-
+	# verify arg type is string
+	if string is isinstance(string, basestring):
+		raise TypeError("Argument must be string")
+		
+	def string_rule_to_sequence(rule = ''):
+		if 'x' in rule:
+			rule = re.split(r"[x,X]", rule)
+			_range = re.split(r"\-", rule[0])
+			return [s for s in range(int(_range[0]),int(_range[-1])) if s-int(_range[0]) % int(rule[-1]) == 0]
+		elif '-' in rule:
+			_range = re.split(r"\-", rule)
+			return range(int(_range[0]),int(_range[-1]))
+		else:
+			return rule
+	
+	return [string_rule_to_sequence(e) for e in re.split(r'\,',re.sub(WHITE_SPACES, "", string))]
 
 
 
@@ -111,11 +128,16 @@ if __name__ == "__main__":
 	del s[5]
 	print(s)
 	print(len(s))
-	ls = [1,2,3,4,5,6,8,10,12,14,16,17,19,20,22,25,28,29,31]
+	ls = [1,2,3,11,12,20,22,24,25]
 	print(ls)
 	#print("")
-	print(sequence_to_range(ls))
+	st = sequence_to_compact_string(ls)
+	print(st)
+	ls = compact_string_to_sequence(st)
+	print(ls)
+	"""
 	print("")
 	l = [4,8,12,16,17,18,22]
 	print(l)
 	print(sequence_to_range(l))
+	"""
